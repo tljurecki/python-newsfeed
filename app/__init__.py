535b0a7ddddc4e6e369e25445e5a910c547846db
Tanya Jurecki <tljurecki@gmail.com>
2022-08-21 15:52:04 -0400

    added filters to format date, url and pluralize words

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from app.routes import home, dashboard
 from flask import Flask
+from app.db import init_db
+from app.utils import filters
 
 def create_app(test_config=None):
     # set up add config
     app = Flask(__name__, static_url_path='/') 
     app.url_map.strict_slashes = False 
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
     app.config.from_mapping(
         SECRET_KEY='super_secret_key'
     ) 
@@ -14,6 +19,7 @@ def create_app(test_config=None):
 
     # register routes
     app.register_blueprint(home)
-    app.register_blueprint(dashboard)
+    app.register_blueprint(dashboard) 
+    init_db(app)
 
     return app
